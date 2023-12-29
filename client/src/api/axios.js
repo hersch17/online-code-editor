63a3a21e84e66782744c1d371bcb5005efe3d6f0
herschelleT <herschellethomas10@gmail.com>
2023-12-29 21:53:30 +0400

    vercel 11

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default axios.create({
-  baseURL: "http://localhost:8080/api/v1",
-  // baseURL:
-  //   "https://online-code-editor-omega.vercel.app",
+  // baseURL: "http://localhost:8080/api/v1",
+  baseURL:
+    "https://online-code-editor-omega.vercel.app",
 });
