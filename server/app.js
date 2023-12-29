b3364ae28777872675ef370b3b5995432e34177a
herschelleT <herschellethomas10@gmail.com>
2023-12-29 21:08:45 +0400

    vercel 10

--- server/app.js
+++ server/app.js
@@ -18,7 +18,9 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan("tiny"));
-
+app.get("/", (req, res) => {
+  res.json("API for online editor");
+});
 app.use("/api/v1/run", codeRouter);
 app.use("/api/v1/users", userRouter);
 
--- server/index.js
+++ server/index.js
@@ -9,9 +9,7 @@ mongoose.connect(DB, {}).then((con) => {
   //console.log(con.connections);
   console.log("DB connection successful");
 });
-app.get("/", (req, res) => {
-  res.json("API for online editor");
-});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
